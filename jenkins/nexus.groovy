a6d46dbabdcccf6f8927821da41aed366dee1649
John Reah <john.reah@johnreah.com>
2021-02-15 12:51:33 +0000

    Update nexus.groovy

--- jenkins/nexus.groovy
+++ jenkins/nexus.groovy
@@ -26,12 +26,12 @@ def findInNexus(Artifact artifact) {
             .openConnection() as HttpURLConnection
     connection.setRequestProperty('Accept', 'application/json')
     if (connection.responseCode == 200) {
-//        def json = connection.inputStream.withCloseable { new JsonSlurper().parse(it) }
-//        def asset = json.items[0].assets.find{it.path.endsWith(artifact.packaging)}
-//        artifact.version = asset.maven2.version
-//        artifact.url = asset.downloadUrl
-//    } else {
-//        throw new Exception("connection error")
+        def json = connection.inputStream.withCloseable { new JsonSlurper().parse(it) }
+        def asset = json.items[0].assets.find{it.path.endsWith(artifact.packaging)}
+        artifact.version = asset.maven2.version
+        artifact.url = asset.downloadUrl
+    } else {
+        throw new Exception("connection error")
     }
 }
 
