09dc81472e8b46f9883539aee2cfc1688166c845
John Reah <john.reah@johnreah.com>
2021-02-15 12:50:44 +0000

    Update nexus.groovy

--- jenkins/nexus.groovy
+++ jenkins/nexus.groovy
@@ -2,7 +2,6 @@
 import groovy.json.JsonSlurper
 import groovy.transform.ToString
 
-//@ToString
 class Artifact {
     String groupId
     String artifactId
@@ -15,26 +14,26 @@ class Artifact {
     }
 }
 
-//def findInNexus(Artifact artifact) {
-//    def connection = new URL(""
-//            + "http://localhost:8081/service/rest"
-//            + '/v1/search'
-//            + "?repository=maven-public"
-//            + "&group=${artifact.groupId}"
-//            + "&name=${artifact.artifactId}"
-//            + "&sort=version"
-//            + "&direction=desc")
-//            .openConnection() as HttpURLConnection
-//    connection.setRequestProperty('Accept', 'application/json')
-//    if (connection.responseCode == 200) {
+def findInNexus(Artifact artifact) {
+    def connection = new URL(""
+            + "http://localhost:8081/service/rest"
+            + '/v1/search'
+            + "?repository=maven-public"
+            + "&group=${artifact.groupId}"
+            + "&name=${artifact.artifactId}"
+            + "&sort=version"
+            + "&direction=desc")
+            .openConnection() as HttpURLConnection
+    connection.setRequestProperty('Accept', 'application/json')
+    if (connection.responseCode == 200) {
 //        def json = connection.inputStream.withCloseable { new JsonSlurper().parse(it) }
 //        def asset = json.items[0].assets.find{it.path.endsWith(artifact.packaging)}
 //        artifact.version = asset.maven2.version
 //        artifact.url = asset.downloadUrl
 //    } else {
 //        throw new Exception("connection error")
-//    }
-//}
+    }
+}
 
 def collectUrls() {
 List<Artifact> artifacts = [
